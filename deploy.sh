c85a7c71adb1a0a25804c3e97dcd16e91be53750
mohammedyasir <mohammed.y@introhive.com>
2021-08-22 14:43:49 +0530

    fix 33

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-docker build -t yasir6403/kub-demo-frontend:latest ./kub-demo-frontend
-docker build -t yasir6403/kub-demo-auth-api:latest  ./kub-demo-auth-api
-docker build -t yasir6403/kub-demo-tasks-api:latest ./kub-demo-tasks-api
-docker build -t yasir6403/kub-demo-users-api:latest ./kub-demo-users-api
+docker build -t yasir6403/kub-demo-frontend:latest ./frontend
+docker build -t yasir6403/kub-demo-auth-api:latest ./auth-api
+docker build -t yasir6403/kub-demo-tasks-api:latest ./tasks-api
+docker build -t yasir6403/kub-demo-users-api:latest ./users-api
 
 docker push yasir6403/kub-demo-frontend:latest
 docker push yasir6403/kub-demo-auth-api:latest
