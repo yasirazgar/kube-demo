0e7863722f9c96b61e20840c81a086cd8e07568e
mohammedyasir <mohammed.y@introhive.com>
2021-08-22 14:10:57 +0530

    typo

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-docker build -t yasir6403/kube-demo-frontend:latest ./kube-demo-frontend
-docker build -t yasir6403/kube-demo-auth-api:latest -t yasir6403/kube-demo-auth-api:$SHA -f ./server/Dockerfile ./server
-docker build -t yasir6403/kube-demo-tasks-api:latest -t yasir6403/kube-demo-auth-api:$SHA -f ./server/Dockerfile ./server
-docker build -t yasir6403/kube-demo-users-api:latest -t yasir6403/kube-demo-auth-api:$SHA -f ./server/Dockerfile ./server
+docker build -t yasir6403/kub-demo-frontend:latest ./kub-demo-frontend
+docker build -t yasir6403/kub-demo-auth-api:latest -t yasir6403/kub-demo-auth-api:$SHA -f ./server/Dockerfile ./server
+docker build -t yasir6403/kub-demo-tasks-api:latest -t yasir6403/kub-demo-auth-api:$SHA -f ./server/Dockerfile ./server
+docker build -t yasir6403/kub-demo-users-api:latest -t yasir6403/kub-demo-auth-api:$SHA -f ./server/Dockerfile ./server
 
-docker push yasir6403/kube-demo-frontend:latest
-docker push yasir6403/kube-demo-auth-api:latest
-docker push yasir6403/kube-demo-tasks-api:latest
-docker push yasir6403/kube-demo-users-api:latest
+docker push yasir6403/kub-demo-frontend:latest
+docker push yasir6403/kub-demo-auth-api:latest
+docker push yasir6403/kub-demo-tasks-api:latest
+docker push yasir6403/kub-demo-users-api:latest
